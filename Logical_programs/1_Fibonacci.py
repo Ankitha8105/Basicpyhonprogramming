4c891215d9499b7fd1492dc61036a9b7acf2c5cf
Ankitha8105 <ankithabl2001@gmail.com>
2024-11-13 12:24:37 +0530

    [Ankitha] Add: Python program to Determine the Fibonacci Series of a number

---
+++ Logical_programs/1_Fibonacci.py
@@ -0,0 +1,37 @@
+'''
+    @Author: Ankitha B L
+    @Date:09 -11-2024
+    @Last Modified by: Ankitha B L
+    @Last Modified time: 1:13
+    @Title : Fibonacci Series problem
+'''
+
+def fact_num(num):
+    
+    """ 
+    Description :
+        This function is used to find the factorial of a number
+    Parameters :
+        num = user entered number
+    Return :
+        It returns the factorial value of a number
+    """
+    n1 = 0
+    n2 = 1
+    print(n1,end= ' ')
+    print(n2,end = ' ')
+ 
+    for i in range(3,num):
+        n3 = n1 + n2
+        n1 = n2
+        n2 = n3
+
+        print(n3,end=' ')
+
+def main():
+    num = int(input("Enter  the number :"))
+
+    fact_num(num)
+
+if __name__ == "__main__":
+    main()
